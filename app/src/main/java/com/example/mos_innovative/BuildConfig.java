4811dfaa1f31d5ba4355a7e5b12111980b5862d5
Lakshin Pathak <97179656+Lbp2563@users.noreply.github.com>
2024-10-22 15:38:00 +0530

    Update BuildConfig.java

--- app/src/main/java/com/example/mos_innovative/BuildConfig.java
+++ app/src/main/java/com/example/mos_innovative/BuildConfig.java
@@ -1,5 +1,5 @@
 package com.example.mos_innovative;
 
 public class BuildConfig {
-    public static String apiKey = "AIzaSyDQfuSj-R7wmULXHh88lFMGoeM9J6KlOCw";
+    public static String apiKey = "Enter Gemini Api Key here";
 }
